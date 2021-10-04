53f19b5ab8515064fe182e04fb99963f870d0174
Richard Seward <raseward14@gmail.com>
2021-10-03 18:22:51 -0600

    URL parameters added POST route upvoting article tested in postman

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,13 +1,41 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// if you are using the latest version of express, you dont have to install body-parser package
+// body parser is now added to express
+// import bodyParser from 'body-parser';
 
-app.use(bodyParser.json());
+// fake database -> JSON object we modify whenever specific route is hit
+// keys of JSON object will be unique name fields of the articles from our front end
+// values will be JSON objects that store information about the articles
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    }, 
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+};
 
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello!'));
-// app.post('/hello', (req, res) => res.send('Hello!'));
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+// MIDDLEWARE
+// app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 
+// app.get('/hello', (req, res) => res.send('Hello!'));
+// app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
+// app.post('/hello', (req, res) => res.send('Hello!'));
+// app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    
+    articlesInfo[articleName].upvotes += 1;
+    // send a response telling the client how many upvotes the articles has
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'));
