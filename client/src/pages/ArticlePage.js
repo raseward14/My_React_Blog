189f54cd89d828d3ca9e83da31bbc91de7a6f759
Richard Seward <raseward14@gmail.com>
2021-10-27 07:45:31 -0600

    added useState and useEffect for articleInfo in articles

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
@@ -8,13 +8,19 @@ function ArticlePage({ match }) {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 })
+    });
+
     if(!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
     
     return (
         <>
-        <h1>{article.title}</h1> 
+        <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvotes!</p> 
         {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
