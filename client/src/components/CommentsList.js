86e2d0acebc85286fc03573a3ddb661ebe267823
Richard Seward <raseward14@gmail.com>
2021-10-30 08:51:09 -0600

    comments displayed on articlePage.js

--- client/src/components/CommentsList.js
+++ client/src/components/CommentsList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 function CommentsList({ comments }) {
-    <>
-    <h3>Comments:</h3>
-    {comments.map((comment, key) => {
-        <div className='comment' key={key}>
-            <h4>{comment.username}</h4>
-            <p>{comment.text}</p>
-        </div>
-    })}
-    </>
+    return(
+        <>
+        <h3>Comments:</h3>
+        {comments.map((comment, key) => (
+            
+            <div className='comment' key={key}>
+                <h4>{comment.username}</h4>
+                <p>{comment.text}</p>
+            </div>
+        ))}
+        </>    
+    )
 };
 export default CommentsList;
